5bd31270fe10a6578a85735f81b623cb6d77d820
Jack Barrett <jjbdev21@gmail.com>
2022-01-27 23:44:54 +0000

    Added MUI

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,33 @@
+import { Container, createTheme, Paper, ScopedCssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
 import React from 'react';
 import './App.css';
 import Router from './Router';
 
-function App() {
-  return (
-    <>
-      <h1>School Time Management App</h1>
-      <Router/>
-    </>
-  );
-}
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 360,
+      md: 960,
+      lg: 1280,
+      xl: 1920
+    }
+  }
+})
 
-export default App;
+export default function App() {
+  const isMobile = useMediaQuery("(max-width: 360px)");
+  return (
+    <ScopedCssBaseline>
+      <ThemeProvider theme={theme}>{
+        isMobile ? 
+        <Router/> : 
+        <Container maxWidth="sm" disableGutters={true}>
+            <Paper>
+                <Router/>
+            </Paper>
+        </Container>
+      }</ThemeProvider>
+    </ScopedCssBaseline>
+  )
+}
