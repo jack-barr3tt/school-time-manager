a90fab9978605816b0ac8169b4b6ee3311826c4a
Jack Barrett <jjbdev21@gmail.com>
2022-01-29 20:03:05 +0000

    Created navigation bar component

---
+++ app/src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { IconButton, Paper, Stack, Typography } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import { grey } from '@mui/material/colors';
+import { useNavigate } from 'react-router-dom';
+
+export const NavBar : FC<{ name: string }> = ({ name }) => {
+    const navigate = useNavigate()
+
+    return <>
+        <Paper sx={{ backgroundImage: "linear-gradient(#c2e7ff, white)", p: 3, width: "100%", position: "absolute", left: 0, top: 0 }} elevation={0}/>
+        <Paper elevation={0} sx={{ position: "absolute", top: 22, left: 0, width: "100%", p: 2, backgroundColor: grey[100], borderRadius: 4 }}>
+            <Stack direction="row" alignItems="center">
+                <IconButton aria-label="back" sx={{ mr: 2 }} onClick={() => navigate(-1)}>
+                    <ArrowBack/>
+                </IconButton>
+                <Typography variant="h5">{name}</Typography>
+            </Stack>
+        </Paper>
+        <Paper elevation={0} sx={{ p: 6 }}/>
+    </>
+}
