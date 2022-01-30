4f89c38ca4a424f078d0b46e18956e4ffe537dbb
Jack Barrett <jjbdev21@gmail.com>
2022-01-30 22:58:29 +0000

    Minor aesthetic changes to navbar

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -9,7 +9,7 @@ export const NavBar : FC<{ name: string }> = ({ name }) => {
 
     return <>
         <Paper sx={{ backgroundImage: "linear-gradient(#c2e7ff, white)", p: 3, width: "100%", position: "absolute", left: 0, top: 0 }} elevation={0}/>
-        <Paper elevation={0} sx={{ position: "absolute", top: 7, left: 0, width: "100%", p: 2, backgroundColor: grey[100], borderRadius: 4 }}>
+        <Paper elevation={0} sx={{ position: "absolute", top: 7, left: 0, width: "100%", p: 2, backgroundColor: grey[50], borderRadius: 4 }}>
             <Stack direction="row" alignItems="center">
                 <IconButton aria-label="back" sx={{ mr: 2 }} onClick={() => navigate(-1)}>
                     <ArrowBack/>
