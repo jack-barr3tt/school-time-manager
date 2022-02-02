b893f8c64ddff3f1d503f7338a52abde8d973c86
jack-barr3tt <jjbdev21@gmail.com>
2022-02-02 19:34:02 +0000

    Back button now works more as expected

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -2,16 +2,21 @@ import React, { FC } from 'react';
 import { IconButton, Paper, Stack, Typography } from '@mui/material';
 import { ArrowBack } from '@mui/icons-material';
 import { grey } from '@mui/material/colors';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const NavBar : FC<{ name: string }> = ({ name }) => {
     const navigate = useNavigate()
+    const location = useLocation()
+
+    const navigateBack = () => {
+        navigate("/" + location.pathname.split("/").filter(x => x.length > 1).slice(0, -1).join("/"))
+    }
 
     return <>
         <Paper sx={{ backgroundImage: "linear-gradient(#c2e7ff, white)", p: 3, width: "100%", position: "absolute", left: 0, top: 0 }} elevation={0}/>
         <Paper elevation={0} sx={{ position: "absolute", top: 7, left: 0, width: "100%", p: 2, backgroundColor: grey[50], borderRadius: 4 }}>
             <Stack direction="row" alignItems="center">
-                <IconButton aria-label="back" sx={{ mr: 2 }} onClick={() => navigate(-1)}>
+                <IconButton aria-label="back" sx={{ mr: 2 }} onClick={navigateBack}>
                     <ArrowBack/>
                 </IconButton>
                 <Typography variant="h5">{name}</Typography>
