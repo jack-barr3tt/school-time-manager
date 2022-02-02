de3490a3f609002cfb2c0f93eaa1c9e33a394f93
jack-barr3tt <jjbdev21@gmail.com>
2022-02-02 20:49:52 +0000

    Homeworks can be created

--- app/src/pages/utility/NewHomework.tsx
+++ app/src/pages/utility/NewHomework.tsx
@@ -2,28 +2,68 @@ import { Save } from '@mui/icons-material';
 import { DatePicker, LocalizationProvider } from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import { Checkbox, Fab, Slider, Stack, TextField, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Subject from '../../api/Subjects';
+import { User } from '../../api/Users';
+import { userContext } from '../../App';
 import CreateableAutocomplete from '../../components/CreatableAutocomplete';
 import { NavBar } from '../../components/NavBar';
 
 export default function NewHomework() {
+    interface SubjectInput {
+        _id?: number;
+        name: string;
+        color?: number;
+        inputValue?: string;
+    }
+    
     const [task, setTask] = useState<string>();
     const [subject, setSubject] = useState<SubjectInput>();
     const [due, setDue] = useState<Date|null>();
     const [difficultyGiven, setDifficultyGiven] = useState<boolean>(false);
     const [difficulty, setDifficulty] = useState<number>();
-
-    interface SubjectInput {
-        id?: string;
-        name: string;
-        color?: number;
-        inputValue?: string;
-    }
-
     const [subjects, setSubjects] = useState<SubjectInput[]>([]);
 
-    const saveHomework = (event: React.FormEvent<HTMLFormElement>) => {
+    const user = useContext(userContext)
+
+    const navigate = useNavigate()
+
+    const fetchSubjects = useCallback(async () => {
+        let fetchedSubjects = await User.forge(user.id).subjects?.get()
+        if(fetchedSubjects) setSubjects(fetchedSubjects)
+    }, [user.id])
+
+    useEffect(() => {
+        fetchSubjects()
+    }, [fetchSubjects])
+
+    const saveHomework = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
+
+        if(!task) return
+        if(!subject) return
+
+        let chosenSubject: Subject|undefined;
+        if(!subject._id) {
+            chosenSubject = await User.forge(user.id).subjects?.create({
+                name: subject.name,
+                color: subject.color
+            })
+            if(chosenSubject) setSubjects([...subjects, chosenSubject])
+        }else{
+            chosenSubject = subject as Subject
+        }
+        if(!chosenSubject) return
+
+        await User.forge(user.id).homework?.create({
+            task,
+            subject_id: chosenSubject._id,
+            due: due?.getTime(),
+            difficulty: difficulty
+        })
+
+        navigate('/homework')
     }
 
     return <>
@@ -31,7 +71,7 @@ export default function NewHomework() {
         <form onSubmit={saveHomework}>
             <Stack direction="column" spacing={2}>
                 <TextField label="Task" fullWidth autoFocus onChange={(e) => setTask(e.target.value)} autoComplete='off'/>
-                <CreateableAutocomplete<SubjectInput> label="Subject" options={subjects} chosenSetter={setSubject} chosen={subject!}/>
+                <CreateableAutocomplete<SubjectInput> label="Subject" options={subjects} chosenSetter={setSubject} chosen={subject!} onOpen={(e) => {fetchSubjects()}}/>
                 <LocalizationProvider dateAdapter={AdapterDateFns}>
                     <DatePicker 
                         label="Due" 
