1db232a1c18153a4e625983f028ae84a8a898741
jack-barr3tt <jjbdev21@gmail.com>
2022-02-02 16:10:11 +0000

    Homework now fetched on load

--- app/src/pages/Homework.tsx
+++ app/src/pages/Homework.tsx
@@ -1,19 +1,29 @@
 import { Add } from '@mui/icons-material';
 import { Fab, Stack } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Homework from '../api/Homework';
+import { User } from '../api/Users';
+import { userContext } from '../App';
 import { HomeworkCard } from '../components/HomeworkCard';
 import { NavBar } from '../components/NavBar';
-import { Homework as HomeworkType } from '../types';
 
-export default function Homework() {
-    const [homework, setHomework] = useState<HomeworkType[]>([])
+export default function HomeworkPage() {
+    const [homework, setHomework] = useState<Homework[]>([])
     const navigate = useNavigate()
+
+    const user = useContext(userContext)
+    
+    useEffect(() => {
+        User.forge(user.id).homework?.get().then(h => {
+            setHomework(h)
+        })
+    }, [user.id])
 
     return <>
         <NavBar name="Homework"/>         
         <Stack direction="column" spacing={3} alignItems="center">
-            {homework.map(hw => <HomeworkCard key={hw.task} homework={hw} />)}
+            {homework.sort((a,b) => a.due.getTime() - b.due.getTime()).map(hw => <HomeworkCard key={hw.task} homework={hw} />)}
         </Stack>
         <Fab color="primary" sx={{ position: "absolute", right: "24px", bottom: "24px" }} onClick={() => navigate("new")}>
             <Add/>
