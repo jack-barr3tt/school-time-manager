594cb5b619e0b9853583399a249b212a6138917c
jack-barr3tt <jjbdev21@gmail.com>
2022-02-03 17:07:29 +0000

    Added loading indicator to homework screen

--- app/src/pages/Homework.tsx
+++ app/src/pages/Homework.tsx
@@ -1,5 +1,5 @@
 import { Add } from '@mui/icons-material';
-import { Fab, Stack } from '@mui/material';
+import { CircularProgress, Fab, Stack } from '@mui/material';
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Homework from '../api/Homework';
@@ -13,17 +13,23 @@ export default function HomeworkPage() {
     const navigate = useNavigate()
 
     const user = useContext(userContext)
+
+    const [loading, setLoading] = useState<boolean>(true)
     
     useEffect(() => {
         User.forge(user.id).homework?.get().then(h => {
             setHomework(h)
+            setLoading(false)
         })
     }, [user.id])
-
+    
     return <>
-        <NavBar name="Homework"/>         
-        <Stack direction="column" spacing={3} alignItems="center">
-            {homework.sort((a,b) => a.due.getTime() - b.due.getTime()).map(hw => <HomeworkCard key={hw.task} homework={hw} />)}
+        <NavBar name="Homework"/>
+        {loading && <Stack direction="column" justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
+                <CircularProgress/>
+            </Stack>
+        } 
+        <Stack direction="column" spacing={3} alignItems="center" sx={{ pt: 2 }}>
         </Stack>
         <Fab color="primary" sx={{ position: "absolute", right: "24px", bottom: "24px" }} onClick={() => navigate("new")}>
             <Add/>
