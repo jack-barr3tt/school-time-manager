01d7dc1b4d75dc1a73c82248d7fc5e575410fbbe
jack-barr3tt <jjbdev21@gmail.com>
2022-02-04 23:11:18 +0000

    Homeworks can now be fetched by id

--- app/src/api/HomeworkManager.ts
+++ app/src/api/HomeworkManager.ts
@@ -15,11 +15,16 @@ export default class HomeworkManager {
         this.userId = userId
     }
 
-    async get() {
-        const { data } = await axios.get(`http://localhost:3000/users/${this.userId}/homework`) as AxiosResponse<Homework[]>
-        return data.map(s => new Homework(s))
+    async get(id?: number) {
+        if(id) {
+            const { data } = await axios.get(`http://localhost:3000/users/${this.userId}/homework/${id}`) as AxiosResponse<Homework>
+            return new Homework(data)
+        }else{
+            const { data } = await axios.get(`http://localhost:3000/users/${this.userId}/homework`) as AxiosResponse<Homework[]>
+            return data.map(s => new Homework(s))
+        }
     }
-
+    
     async create(homework: HomeworkCreate) {
         const { data } = await axios.post(`http://localhost:3000/users/${this.userId}/homework`, homework)
         return data as Homework
