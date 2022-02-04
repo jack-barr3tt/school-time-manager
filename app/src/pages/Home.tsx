0c04b268479048a607a7f7f94f9fc860662c5122
jack-barr3tt <jjbdev21@gmail.com>
2022-02-04 23:12:17 +0000

    Personalised welcome message

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,13 +1,16 @@
 import { Button, Paper, Typography } from '@mui/material';
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { userContext } from '../App';
 
 export default function Home() {
     const navigate = useNavigate()
 
+    const user = useContext(userContext)
+
     return <>
         <Paper sx={{ backgroundImage: "linear-gradient(#c2e7ff, white)", p: 3, width: "100%", position: "absolute", left: 0, top: 0 }} elevation={0}/>
-        <Typography variant="h5" textAlign="center" sx={{ zIndex: 1 }}>Welcome</Typography>
+        <Typography variant="h5" textAlign="center" sx={{ zIndex: 1 }}>{user.username ? `Welcome, ${user.username}` : "Welcome"}</Typography>
         <Button variant="outlined" fullWidth onClick={() => navigate("timetable")}>Timetable</Button>
         <Button variant="outlined" fullWidth onClick={() => navigate("homework")}>Homework</Button>
         <Button variant="outlined" fullWidth onClick={() => navigate("setup")}>Setup</Button>
