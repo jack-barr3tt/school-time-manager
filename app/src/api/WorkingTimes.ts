03d86686197e38a6ad8bfc8ea0e0ca1bbc9eab57
jack-barr3tt <jjbdev21@gmail.com>
2022-02-07 23:18:43 +0000

    Working times are now displayed

--- app/src/api/WorkingTimes.ts
+++ app/src/api/WorkingTimes.ts
@@ -1,3 +1,8 @@
+const getComponents = (date: Date) => {
+    const main = (""+date).split(".")[0]
+    return main.split(":").map(r => parseInt(r))    
+}
+
 export default class WorkingTime {
     readonly _id: number;
     readonly start_time: Date;
@@ -5,7 +10,7 @@ export default class WorkingTime {
 
     constructor(data: WorkingTime) {
         this._id = data._id
-        this.start_time = data.start_time
-        this.end_time = data.end_time
+        this.start_time = new Date(0,0,0, ...getComponents(data.start_time))
+        this.end_time = new Date(new Date(0,0,0, ...getComponents(data.end_time)))
     }
 }
