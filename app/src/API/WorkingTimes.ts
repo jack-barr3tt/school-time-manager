682fe91ab65208148ea029a6c637efc040bc65c4
jack-barr3tt <jjbdev21@gmail.com>
2022-02-08 21:03:39 +0000

    Improved working time api wrapper

--- app/src/API/WorkingTimes.ts
+++ app/src/API/WorkingTimes.ts
@@ -24,7 +24,10 @@ export default class WorkingTime {
     }
 
     async edit(newData: Partial<WorkingTime>) {
-        const { data } = await axios.patch(`http://localhost:3000/users/${this.user_id}/times/${this._id}`, newData)
+        const { data } = await axios.patch(`http://localhost:3000/users/${this.user_id}/times/${this._id}`, {
+            start_time: newData.start_time?.getTime(),
+            end_time: newData.end_time?.getTime()
+        })
         return data as WorkingTime
     }
 }
--- app/src/API/WorkingTimesManager.ts
+++ app/src/API/WorkingTimesManager.ts
@@ -13,9 +13,17 @@ export default class WorkingTimesManager {
         this.userId = userId
     }
 
-    async get() {
-        const { data } = await axios.get<WorkingTime[]>(`http://localhost:3000/users/${this.userId}/times`)
-        return data.map(s => new WorkingTime(s))
+    async get() : Promise<WorkingTime[]>
+    async get(id: number) : Promise<WorkingTime> 
+    async get(id?: number)
+    {
+        if(id) {
+            const { data } = await axios.get<WorkingTime>(`http://localhost:3000/users/${this.userId}/times/${id}`)
+            return new WorkingTime(data)
+        }else{
+            const { data } = await axios.get<WorkingTime[]>(`http://localhost:3000/users/${this.userId}/times`)
+            return data.map(s => new WorkingTime(s))
+        }   
     }
 
     async create(time: WorkingTimeCreate) {
