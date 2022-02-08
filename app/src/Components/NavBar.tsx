5db575dc8dd28e6f5e3cf1b845f60077f95e02ca
jack-barr3tt <jjbdev21@gmail.com>
2022-02-08 21:03:07 +0000

    Navbar can alternatively call a function on back

--- app/src/Components/NavBar.tsx
+++ app/src/Components/NavBar.tsx
@@ -3,8 +3,8 @@ import { ArrowBack } from '@mui/icons-material';
 import { grey } from '@mui/material/colors';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-export default function NavBar (props: { name: string }) {
-    const { name } = props
+export default function NavBar (props: { name: string, onBack?: () => void }) {
+    const { name, onBack } = props
     const navigate = useNavigate()
     const location = useLocation()
 
@@ -16,7 +16,7 @@ export default function NavBar (props: { name: string }) {
         <Paper sx={{ zIndex: 10, backgroundImage: "linear-gradient(#c2e7ff, white)", p: 3, width: "100%", position: "absolute", left: 0, top: 0 }} elevation={0}/>
         <Paper elevation={0} sx={{ zIndex: 11, position: "absolute", top: 7, left: 0, width: "100%", p: 2, backgroundColor: grey[50], borderRadius: 4 }}>
             <Stack direction="row" alignItems="center">
-                <IconButton aria-label="back" sx={{ mr: 2 }} onClick={navigateBack}>
+                <IconButton aria-label="back" sx={{ mr: 2 }} onClick={onBack || navigateBack}>
                     <ArrowBack/>
                 </IconButton>
                 <Typography variant="h5">{name}</Typography>
