f788fafbdd433c37b63de50ef50db82180c36967
jack-barr3tt <jjbdev21@gmail.com>
2022-02-09 18:12:26 +0000

    Improvements to time range picker

--- app/src/Components/TimeRangePicker.tsx
+++ app/src/Components/TimeRangePicker.tsx
@@ -1,44 +1,51 @@
 import { Save } from '@mui/icons-material';
 import { TimePicker } from '@mui/lab';
-import { Stack, TextField, Fab } from '@mui/material';
-import { Dispatch, FormEvent, SetStateAction } from 'react';
+import { TextField, Fab } from '@mui/material';
+import { add } from 'date-fns';
+import { Dispatch, SetStateAction, useEffect } from 'react';
 
-type StoredDate = Date|null|undefined
+export type OptionalDate = Date|null|undefined
 
 type Props = {
-    startTime: StoredDate;
-    setStartTime: Dispatch<SetStateAction<StoredDate>>;
-    endTime: StoredDate;
-    setEndTime: Dispatch<SetStateAction<StoredDate>>;
-    onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+    startTime: OptionalDate;
+    setStartTime: Dispatch<SetStateAction<OptionalDate>>;
+    endTime: OptionalDate;
+    setEndTime: Dispatch<SetStateAction<OptionalDate>>;
+    autoDifference?: Duration;
 }
 
 export default function TimeRangePicker(props: Props) {
-    const { startTime, setStartTime, endTime, setEndTime, onSubmit } = props
-    return  <form onSubmit={onSubmit}>
-        <Stack direction="column" spacing={2}>
-            <TimePicker
-                label="Start"
-                renderInput={(params) => <TextField {...params} />}
-                onChange={(e) => setStartTime(e)}
-                value={startTime}
-            />
-            <TimePicker
-                label="End"
-                renderInput={(params) => <TextField {...params} />}
-                onChange={(e) => setEndTime(e)}
-                value={endTime}
-                minTime={startTime}
-                disabled={startTime == null}
-            />
-            <Fab 
-                color="primary" 
-                sx={{ position: "absolute", right: "24px", bottom: "24px" }} 
-                type="submit" 
-                disabled={startTime == null || endTime == null}
-            >
-                <Save/>
-            </Fab>
-        </Stack>
-    </form>;
+    const { startTime, setStartTime, endTime, setEndTime, autoDifference } = props
+
+    useEffect(() => {
+        setStartTime(new Date())
+        if(autoDifference)
+            setEndTime(add(new Date(), autoDifference))
+        else
+            setEndTime(add(new Date(), { hours: 1 }))
+    }, [setStartTime, setEndTime, autoDifference])
+
+    return <>
+        <TimePicker
+            label="Start"
+            renderInput={(params) => <TextField {...params} />}
+            onChange={(e) => { setStartTime(e); if(autoDifference && e) setEndTime(add(e, autoDifference)) } }
+            value={startTime}
+        />
+        <TimePicker
+            label="End"
+            renderInput={(params) => <TextField {...params} />}
+            onChange={(e) => setEndTime(e) }
+            value={endTime}
+            minTime={startTime}
+            disabled={startTime == null}
+        />
+        <Fab 
+            color="primary" 
+            sx={{ position: "absolute", right: "24px", bottom: "24px" }} 
+            type="submit" 
+        >
+            <Save/>
+        </Fab>
+    </>
 }
