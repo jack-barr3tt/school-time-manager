14efd6be211f87e054e58176523b104d802926e1
jack-barr3tt <jjbdev21@gmail.com>
2022-02-12 19:11:46 +0000

    Homeworks can be edited or deleted

---
+++ app/src/Pages/Homework/EditHomework.tsx
@@ -0,0 +1,129 @@
+import { Save } from "@mui/icons-material";
+import { DatePicker } from "@mui/lab";
+import { Stack, TextField, Typography, Checkbox, Slider, Fab } from "@mui/material";
+import { useState, useContext, useCallback, useEffect, FormEvent } from "react";
+import Homework from "../../API/Homework";
+import Subject from "../../API/Subjects";
+import { User } from "../../API/Users";
+import { userContext } from "../../App";
+import CreateableAutocomplete from "../../Components/CreatableAutocomplete";
+import CreateSubject from "../../Components/CreateSubject";
+import NavBar from "../../Components/NavBar";
+
+type SubjectInput = {
+    _id?: number;
+    name: string;
+    color?: number;
+    inputValue?: string;
+}
+
+type Props = {
+    id?: number;
+    back: () => void;
+}
+
+export default function EditHomework(props: Props) {
+    const { id, back } = props;
+
+    const [task, setTask] = useState<string>("");
+    const [subject, setSubject] = useState<SubjectInput>();
+    const [due, setDue] = useState<Date|null>();
+    const [difficultyGiven, setDifficultyGiven] = useState<boolean>(false);
+    const [difficulty, setDifficulty] = useState<number>();
+    const [subjects, setSubjects] = useState<SubjectInput[]>([]);
+    const [homework, setHomework] = useState<Homework>()
+
+    const user = useContext(userContext);
+
+    const fetchSubjects = useCallback(async () => {
+        let fetchedSubjects = await User.forge(user.id).subjects?.get();
+        if (fetchedSubjects) setSubjects(fetchedSubjects);
+    }, [user.id]);
+
+    const fetchHomework = useCallback(async () => {
+        if(id) {
+            let tempHomework = await User.forge(user.id).homework?.get(id);
+            if (tempHomework) {
+                setHomework(tempHomework)
+                setTask(tempHomework.task);
+                setSubject(tempHomework.subject);
+                setDue(tempHomework.due);
+                setDifficultyGiven(tempHomework.difficulty != null);
+                setDifficulty(tempHomework.difficulty);
+            }
+        }
+    }, [user.id, id]);
+
+    useEffect(() => {
+        fetchSubjects();
+        fetchHomework();
+    }, [fetchSubjects, fetchHomework]);
+
+    const saveHomework = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if(homework && task && subject && due && difficulty) {
+            try{
+                await homework.edit({
+                    task,
+                    subject: { _id: subject._id} as Subject,
+                    due,
+                    difficulty
+                })
+                back()
+            }catch{}
+        }
+    }
+
+    return <>
+        <NavBar name="Edit Homework" onBack={back}/>
+        <form onSubmit={saveHomework}>
+            <Stack direction="column" spacing={2}>
+                <TextField 
+                    fullWidth
+                    autoFocus
+                    label="Task"
+                    autoComplete='off'
+                    onChange={(e) => setTask(e.target.value)} 
+                    value={task}
+                />
+                <CreateableAutocomplete<SubjectInput> 
+                    label="Subject" 
+                    options={subjects} 
+                    chosenSetter={setSubject} 
+                    chosen={subject!} 
+                    onOpen={() => {fetchSubjects()}}
+                    CreateDialog={CreateSubject}
+                />
+                <DatePicker 
+                    label="Due" 
+                    value={due} 
+                    onChange={(newValue) => setDue(newValue)}
+                    renderInput={(props) => <TextField {...props} label="Due" fullWidth/>}
+                />
+                <Typography variant="h6" id="difficulty-label">Difficulty</Typography>
+                <Stack direction="row" alignItems="center">
+                    <Checkbox 
+                        checked={difficultyGiven} 
+                        onChange={(event) => setDifficultyGiven(event.target.checked)}
+                        sx={{ mr: 2}}
+                    />
+                    <Slider
+                        aria-labelledby="difficulty-label"
+                        valueLabelFormat={(value) => ["Easy", "Medium", "Hard"][value]}
+                        onChangeCommitted={(_e,v) => setDifficulty(v as number)}
+                        disabled={!difficultyGiven}
+                        defaultValue={1}
+                        valueLabelDisplay="auto"
+                        step={1}
+                        min={0}
+                        max={2}
+                    />
+                </Stack>
+            </Stack>
+            <Fab color="primary" sx={{ position: "absolute", right: "24px", bottom: "24px" }} type="submit">
+                <Save/>
+            </Fab>
+        </form>
+    </>;
+}
--- app/src/Pages/Homework/ViewHomework.tsx
+++ app/src/Pages/Homework/ViewHomework.tsx
@@ -1,5 +1,5 @@
-import { AccessTime, Assignment, Circle, Event } from '@mui/icons-material';
-import { CircularProgress, Stack, Typography } from '@mui/material';
+import { AccessTime, Assignment, Circle, Delete, Edit, Event } from '@mui/icons-material';
+import { CircularProgress, IconButton, Stack, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import { addDays, format, formatDistanceToNow, isAfter, startOfDay } from 'date-fns';
 import { useCallback, useContext, useEffect, useState } from 'react';
@@ -8,10 +8,13 @@ import Homework from '../../API/Homework';
 import { User } from '../../API/Users';
 import { userContext } from '../../App';
 import NavBar from '../../Components/NavBar';
+import EditHomework from './EditHomework';
 
 export default function ViewHomework() {
     const [homework, setHomework] = useState<Homework>()
     const [loading, setLoading] = useState<boolean>(true)
+    const [editing, setEditing] = useState<boolean>(false)
+    const [editingId, setEditingId] = useState<number>()
     const { id } = useParams()
     const user = useContext(userContext)
     const navigate = useNavigate()
@@ -31,7 +34,7 @@ export default function ViewHomework() {
 
     useEffect(() => {
         fetchHomework()
-    }, [fetchHomework])
+    }, [fetchHomework, editing])
 
     const getColor = (color?: number) => {
         if(!color) return grey[500];
@@ -48,43 +51,64 @@ export default function ViewHomework() {
         return {}
     }
 
-    return <>
-        <NavBar name="View Homework"/>
-        { loading && <Stack direction="column" justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
-                <CircularProgress/>
-            </Stack>
+    const deleteHomework = async () => {
+        if(homework) {
+            await homework.delete()
+            navigate("/homework")
         }
+    }
 
-        { homework?.task && <>
-            <Stack direction="row" spacing={3} alignItems="center" sx={{ pt: 2 }}>
-                <Assignment/>
-                <Typography variant="h5">Task</Typography>
-            </Stack>
-            <Typography variant="body1">{homework.task}</Typography>
-        </> }
+    return <>
+        { !editing ? <>
+            <NavBar name="View Homework" controls={[
+                <IconButton onClick={() => {
+                    setEditing(true)
+                    setEditingId(homework?._id)
+                }}>
+                    <Edit/>
+                </IconButton>,
+                <IconButton onClick={() => deleteHomework()}>
+                    <Delete/>
+                </IconButton>
+            ]}/>
+            { loading && <Stack direction="column" justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
+                    <CircularProgress/>
+                </Stack>
+            }
 
-        { homework?.subject?.name && <>
-            <Stack direction="row" spacing={3} alignItems="center" sx={{ pt: 2 }}>
-                <Circle sx={{ color: `#${getColor(homework?.subject?.color)}`, mr: 1 }} />
-                <Typography variant="h5">Subject</Typography>
-            </Stack>
-            <Typography variant="body1">{homework?.subject?.name}</Typography>
-        </> }
+            { homework?.task && <>
+                <Stack direction="row" spacing={3} alignItems="center" sx={{ pt: 2 }}>
+                    <Assignment/>
+                    <Typography variant="h5">Task</Typography>
+                </Stack>
+                <Typography variant="body1">{homework.task}</Typography>
+            </> }
 
-        { homework?.due != null && <>
-            <Stack direction="row" spacing={3} alignItems="center" sx={{ pt: 2 }}>
-                <Event sx={getDueColor(homework?.due)}/>
-                <Typography variant="h5" sx={getDueColor(homework?.due)}>Due</Typography>
-            </Stack>
-            <Typography variant="body1" sx={getDueColor(homework?.due)}>{`${format(homework?.due, "dd/MM/yy")} (${formatDistanceToNow(homework?.due)})`}</Typography>
-        </> }
+            { homework?.subject?.name && <>
+                <Stack direction="row" spacing={3} alignItems="center" sx={{ pt: 2 }}>
+                    <Circle sx={{ color: `#${getColor(homework?.subject?.color)}`, mr: 1 }} />
+                    <Typography variant="h5">Subject</Typography>
+                </Stack>
+                <Typography variant="body1">{homework?.subject?.name}</Typography>
+            </> }
 
-        { homework?.difficulty && <>
-            <Stack direction="row" spacing={3} alignItems="center" sx={{ pt: 2 }}>
-                <AccessTime/>
-                <Typography variant="h5">Difficulty</Typography>
-            </Stack>
-            <Typography variant="body1">{["Easy","Medium","Hard"][homework?.difficulty || 0]}</Typography>
-        </> }
-    </>;
+            { homework?.due != null && <>
+                <Stack direction="row" spacing={3} alignItems="center" sx={{ pt: 2 }}>
+                    <Event sx={getDueColor(homework?.due)}/>
+                    <Typography variant="h5" sx={getDueColor(homework?.due)}>Due</Typography>
+                </Stack>
+                <Typography variant="body1" sx={getDueColor(homework?.due)}>{`${format(homework?.due, "dd/MM/yy")} (${formatDistanceToNow(homework?.due)})`}</Typography>
+            </> }
+
+            { homework?.difficulty && <>
+                <Stack direction="row" spacing={3} alignItems="center" sx={{ pt: 2 }}>
+                    <AccessTime/>
+                    <Typography variant="h5">Difficulty</Typography>
+                </Stack>
+                <Typography variant="body1">{["Easy","Medium","Hard"][homework?.difficulty || 0]}</Typography>
+            </> }
+        </>
+        :
+        <EditHomework back={() => setEditing(false)} id={editingId}/>
+    }</>
 }
