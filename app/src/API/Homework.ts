bacd6c52a090a20b1989761aff1f1e28cd0a0cd7
jack-barr3tt <jjbdev21@gmail.com>
2022-02-12 19:10:58 +0000

    Delete and edit methods for homework

--- app/src/API/Homework.ts
+++ app/src/API/Homework.ts
@@ -1,7 +1,9 @@
+import axios from "axios";
 import Subject from "./Subjects";
 
 export default class Homework {
     readonly _id: number;
+    private user_id: number;
     readonly task: string;
     readonly subject: Subject;
     readonly due?: Date;
@@ -9,9 +11,25 @@ export default class Homework {
 
     constructor(data: Homework) {
         this._id = data._id
+        this.user_id = data.user_id
         this.task = data.task
         this.subject = new Subject(data.subject)
         this.due = data.due == null ? undefined : new Date(data.due)
         this.difficulty = data.difficulty
+    }
+
+    async delete() {
+        const { data } = await axios.delete(`http://localhost:3000/users/${this.user_id}/homework/${this._id}`)
+        return data
+    }
+
+    async edit(newData: Partial<Homework>) {
+        const { data } = await axios.patch<Homework>(`http://localhost:3000/users/${this.user_id}/homework/${this._id}`, {
+            task: newData.task,
+            subject_id: newData.subject?._id,
+            due: newData.due?.getTime(),
+            difficulty: newData.difficulty
+        })
+        return data
     }
 }
