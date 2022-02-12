249ceb2f1bd98e6631ef5cbded2ca34ed44ca230
jack-barr3tt <jjbdev21@gmail.com>
2022-02-12 19:11:15 +0000

    Navbar can render action buttons

--- app/src/Components/NavBar.tsx
+++ app/src/Components/NavBar.tsx
@@ -2,9 +2,16 @@ import { IconButton, Paper, Stack, Typography } from '@mui/material';
 import { ArrowBack } from '@mui/icons-material';
 import { grey } from '@mui/material/colors';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { ReactNode } from 'react';
 
-export default function NavBar (props: { name: string, onBack?: () => void }) {
-    const { name, onBack } = props
+type Props = {
+    name: string;
+    onBack?: () => void
+    controls?: ReactNode[];
+}
+
+export default function NavBar (props: Props) {
+    const { name, onBack, controls } = props
     const navigate = useNavigate()
     const location = useLocation()
 
@@ -15,12 +22,25 @@ export default function NavBar (props: { name: string, onBack?: () => void }) {
     return <>
         <Paper sx={{ zIndex: 10, backgroundImage: "linear-gradient(#c2e7ff, white)", p: 3, width: "100%", position: "absolute", left: 0, top: 0 }} elevation={0}/>
         <Paper elevation={0} sx={{ zIndex: 11, position: "absolute", top: 7, left: 0, width: "100%", p: 2, backgroundColor: grey[50], borderRadius: 4 }}>
-            <Stack direction="row" alignItems="center">
+            { controls ?
+                <Stack direction="row" alignItems="center" justifyContent="space-between">
+                    <Stack direction="row" alignItems="center">
+                        <IconButton aria-label="back" sx={{ mr: 2 }} onClick={onBack || navigateBack}>
+                            <ArrowBack/>
+                        </IconButton>                
+                        <Typography variant="h5">{name}</Typography>
+                    </Stack>
+                    <Stack direction="row" alignItems="center">
+                        {controls}
+                    </Stack>
+                </Stack>
+            : <Stack direction="row" alignItems="center">
                 <IconButton aria-label="back" sx={{ mr: 2 }} onClick={onBack || navigateBack}>
                     <ArrowBack/>
                 </IconButton>
                 <Typography variant="h5">{name}</Typography>
             </Stack>
+            }
         </Paper>
         <Paper elevation={0} sx={{ p: 5 }}/>
     </>
