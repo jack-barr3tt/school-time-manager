abde582bb3befe281ba52428357282dcb50d102e
jack-barr3tt <jjbdev21@gmail.com>
2022-02-14 16:25:02 +0000

    Swipe actions for homeworks

--- app/src/Components/HomeworkCard.tsx
+++ app/src/Components/HomeworkCard.tsx
@@ -1,14 +1,23 @@
-import { AccessTime, Circle } from '@mui/icons-material';
-import { ButtonBase, Paper, Stack, Typography } from '@mui/material';
+import { AccessTime, Check, Circle, Delete } from '@mui/icons-material';
+import { ButtonBase, Paper, Stack, styled, Typography, useTheme } from '@mui/material';
 import { grey, red } from '@mui/material/colors';
 import { useNavigate } from 'react-router-dom';
+import SwipeableViews from "react-swipeable-views"
 import Homework from '../API/Homework';
 
-export default function HomeworkCard (props: { homework: Homework }) {
+type Props = {
+    homework: Homework;
+    _delete: () => void;
+    complete: () => void;
+}
+
+export default function HomeworkCard (props: Props) {
+    const { _delete, complete } = props;
     const { _id, task, subject, due, difficulty } = props.homework;
     const { name, color } = subject;
     
     const navigate = useNavigate()
+    const theme = useTheme()
 
     const getMonth = (date: Date) => {
         const months = [
@@ -49,27 +58,56 @@ export default function HomeworkCard (props: { homework: Homework }) {
         return text.substring(0, limit).split(" ").slice(0, -1).join(" ") + "..."
     }
 
-    return <ButtonBase onClick={() => navigate(""+_id)} sx={{ borderRadius: 6, boxSizing: "border-box", width: "100%" }}>
-        <Paper elevation={4} sx={{ p: 2, borderRadius: 6, width: "100%" }}>
-            <Stack direction="column">
-                <Stack direction="row" alignItems="center" justifyContent="space-between">
-                    <Typography variant="h5" sx={{ my: 2, pr: 2, textAlign: "left" }}>{truncateText(task)}</Typography>
-                    {due && <Paper elevation={0} sx={{ height: "96px", aspectRatio: "1", backgroundColor: grey[200], borderRadius: 3 }}>
-                        <Paper sx={{ width: "100%", height: "24px", borderRadius: "12px 12px 0 0", backgroundColor: red[500] }} elevation={0}>
-                            <Typography variant="body1" sx={{ textAlign: "center", height: "100%" }} color="white">{getMonth(due)}</Typography>
-                        </Paper>
-                        <Typography variant="body1" sx={{ textAlign: "center", fontSize: "2.5rem", my: "6px" }}>{due.getDate()}</Typography>
-                    </Paper>}
+    const displayCardTheme = { borderRadius: 0, width: 1, height: 1, display: "flex", flexDirection: "row", alignItems: "center", px: 2 }
+    const completeDisplay = <Paper sx={{ backgroundColor: "success.main", justifyContent: "flex-start", ...displayCardTheme}}>
+        <Check sx={{ width: "3rem", height: "3rem"}}/>
+    </Paper>
+
+    const deleteDisplay = <Paper sx={{ backgroundColor: "error.main", justifyContent: "flex-end", ...displayCardTheme}}>
+        <Delete sx={{ width: "3rem", height: "3rem"}}/>
+    </Paper>
+
+    const StyledSwiper = styled(SwipeableViews)({
+        borderRadius: +theme.shape.borderRadius * 6,
+        boxShadow: theme.shadows[4]
+    })
+
+    return <StyledSwiper
+                enableMouseEvents
+                index={1}
+                hysteresis={1}
+                onChangeIndex={(index) => {
+                    if(index === 0) {
+                        _delete()
+                    }else if(index === 2) {
+                        complete()
+                    }
+                }}
+            >
+            {deleteDisplay}
+            <ButtonBase onClick={() => navigate(""+_id)} sx={{ borderRadius: 6, boxSizing: "border-box", width: "100%" }}>
+            <Paper elevation={0} sx={{ p: 2, width: 1, height: 1 }}>
+                <Stack direction="column">
+                    <Stack direction="row" alignItems="center" justifyContent="space-between">
+                        <Typography variant="h5" sx={{ my: 2, pr: 2, textAlign: "left" }}>{truncateText(task)}</Typography>
+                        {due && <Paper elevation={0} sx={{ height: "96px", aspectRatio: "1", backgroundColor: grey[200], borderRadius: 3 }}>
+                            <Paper sx={{ width: "100%", height: "24px", borderRadius: "12px 12px 0 0", backgroundColor: red[500] }} elevation={0}>
+                                <Typography variant="body1" sx={{ textAlign: "center", height: "100%" }} color="white">{getMonth(due)}</Typography>
+                            </Paper>
+                            <Typography variant="body1" sx={{ textAlign: "center", fontSize: "2.5rem", my: "6px" }}>{due.getDate()}</Typography>
+                        </Paper>}
+                    </Stack>
+                    <Stack direction="row" alignItems="center">
+                        <Circle sx={{ color: `#${getColor(color)}`, mr: 1 }} />
+                        <Typography variant="body1">{name}</Typography>
+                        { difficulty != null && <>
+                            <AccessTime sx={{ ml: 3, mr: 1 }}/>
+                            <Typography variant="body1">{getDifficulty(difficulty)}</Typography>
+                        </>}
+                    </Stack>
                 </Stack>
-                <Stack direction="row" alignItems="center">
-                    <Circle sx={{ color: `#${getColor(color)}`, mr: 1 }} />
-                    <Typography variant="body1">{name}</Typography>
-                    { difficulty != null && <>
-                        <AccessTime sx={{ ml: 3, mr: 1 }}/>
-                        <Typography variant="body1">{getDifficulty(difficulty)}</Typography>
-                    </>}
-                </Stack>
-            </Stack>
-        </Paper>
-    </ButtonBase>   
+            </Paper>
+        </ButtonBase>
+        {completeDisplay}
+    </StyledSwiper> 
 }
--- app/src/Pages/Homework/Homework.tsx
+++ app/src/Pages/Homework/Homework.tsx
@@ -1,6 +1,6 @@
 import { Add } from '@mui/icons-material';
 import { CircularProgress, Fab, Stack } from '@mui/material';
-import { useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Homework from '../../API/Homework';
 import { User } from '../../API/Users';
@@ -15,13 +15,33 @@ export default function HomeworkPage() {
     const user = useContext(userContext)
 
     const [loading, setLoading] = useState<boolean>(true)
+
+    const loadHomework = useCallback(async () => {
+        console.log("loading homework")
+        let tempHomework = await User.forge(user.id).homework?.get()
+        if(tempHomework) {
+            setHomework(tempHomework)
+            setLoading(false)
+        }
+    }, [user.id])
     
     useEffect(() => {
-        User.forge(user.id).homework?.get().then(h => {
-            setHomework(h)
-            setLoading(false)
-        })
-    }, [user.id])
+        loadHomework()    
+    }, [loadHomework])
+
+    const deleteHomework = async (hw: Homework) => {
+        await hw.delete()
+        setHomework(homework.filter(h => h._id !== hw._id))
+    }
+
+    const completeHomework = async (hw: Homework) => {
+        await hw.markComplete()
+        let replacement = await User.forge(user.id).homework?.get(hw._id)
+        if(replacement) setHomework([
+            ...homework.filter(h => h._id !== hw._id),
+            replacement
+        ])
+    }
     
     return <>
         <NavBar name="Homework"/>
@@ -30,11 +50,11 @@ export default function HomeworkPage() {
             </Stack>
         } 
         <Stack direction="column" spacing={3} alignItems="center" sx={{ pt: 2 }}>
-            {homework.sort((a,b) => {
+            {homework.filter(h => !h.complete).sort((a,b) => {
                 if(!a.due) return 0
                 if(!b.due) return 0
                 return a.due.getTime() - b.due.getTime()
-            }).map(hw => <HomeworkCard key={hw.task} homework={hw} />)}
+            }).map(hw => <HomeworkCard key={hw._id} homework={hw} _delete={() => deleteHomework(hw)} complete={() => completeHomework(hw)} />)}
         </Stack>
         <Fab color="primary" sx={{ position: "absolute", right: "24px", bottom: "24px" }} onClick={() => navigate("new")}>
             <Add/>
