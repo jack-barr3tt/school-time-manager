ca6a397f4d0602af09a31344ab100b5842361858
Jack Barrett <jjbdev21@gmail.com>
2022-02-24 16:47:00 +0000

    Location names can be edited

---
+++ server/src/controllers/location/edit.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { Location } from "../../database/location";
+import { APIError } from "../../errors/types";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+    const { userId, id } = req.params;
+    try{
+        const location = await Location.findById(id, userId);
+        if(!location) throw new APIError("Location not found", 404);
+
+        let { name } = req.body;
+
+        if(name) location.name = name;
+
+        await location.save();
+        res.json(location);
+    }catch(err){
+        res.locals.error = err;
+        next();
+    }
+};
--- server/src/routes/locations.ts
+++ server/src/routes/locations.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import create from "../controllers/location/create";
 import _delete from "../controllers/location/delete";
+import edit from "../controllers/location/edit";
 import get from "../controllers/location/get";
 import getByUser from "../controllers/location/getByUser";
 
@@ -9,6 +10,7 @@ const router = Router({ mergeParams: true })
 router.post('/', create)
 router.get('/', getByUser)
 router.get('/:id', get)
+router.patch('/:id', edit)
 router.delete('/:id', _delete)
 
 export default router
