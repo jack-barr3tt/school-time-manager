33759219f32e63b1d60b569af3fb1c28b39d0278
Jack Barrett <jjbdev21@gmail.com>
2022-02-24 21:06:03 +0000

    All props of timetable can be edited from new page

--- app/src/Pages/Timetable/NewTimetable.tsx
+++ app/src/Pages/Timetable/NewTimetable.tsx
@@ -7,10 +7,10 @@ import Teacher, { TeacherInput } from "../../API/Teacher"
 import { User } from '../../API/Users';
 import { userContext } from '../../App';
 import CreateableAutocomplete from '../../Components/CreatableAutocomplete';
-import CreateSubject from '../Subjects/CreateSubject';
 import NavBar from '../../Components/NavBar';
 import { Save } from '@mui/icons-material';
 import Repeat from '../../API/Repeat';
+import EasyDialog from '../../Components/EasyDialog';
 
 type Props = {
     block: LessonBlock;
@@ -29,6 +29,13 @@ export default function NewTimetable(props: Props) {
     const [subjects, setSubjects] = useState<Subject[]>()
     const [locations, setLocations] = useState<Location[]>()
     const [teachers, setTeachers] = useState<Teacher[]>()
+
+    const [newSubjectText, setNewSubjectText] = useState<string>()
+    const [subjectDialogOpen, setSubjectDialogOpen] = useState<boolean>(false)
+
+    const [subjectEditing, setSubjectEditing] = useState<SubjectInput>()
+    const [locationEditing, setLocationEditing] = useState<LocationInput>()
+    const [teacherEditing, setTeacherEditing] = useState<TeacherInput>()
 
     const user = useContext(userContext)
 
@@ -68,52 +75,125 @@ export default function NewTimetable(props: Props) {
     const saveTimetable = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        if(subject && block && repeat) {           
-            let chosenSubject: Subject|undefined;
-            if(!subject._id) {
-                chosenSubject = await User.forge(user.id).subjects?.create({
-                    name: subject.name,
-                    color: subject.color
-                })
-                if(chosenSubject && subjects) setSubjects([...subjects, chosenSubject])
-            }else{
-                chosenSubject = subject as Subject
-            }
-            if(!chosenSubject) return
-
-            let chosenLocation: Location|undefined;
-            if(location) {
-                if(!location._id) {
-                    chosenLocation = await User.forge(user.id).locations?.create(location.name)
-                    if(chosenLocation && locations) setLocations([...locations, chosenLocation])
-                }else{
-                    chosenLocation = location as Location
-                }
-                if(!chosenLocation) return
-            }
-
-            let chosenTeacher: Teacher|undefined;
-            if(teacher) {
-                if(!teacher._id) {
-                    chosenTeacher = await User.forge(user.id).teachers?.create(teacher.name)
-                    if(chosenTeacher && teachers) setTeachers([...teachers, chosenTeacher])
-                }else{
-                    chosenTeacher = teacher as Teacher
-                }
-                if(!chosenTeacher) return
-            }
-
+        if(subject && subject._id && block && repeat) {           
             await User.forge(user.id).lessons?.create({
-                subject_id: chosenSubject._id,
+                subject_id: subject._id,
                 block_id: block._id,
-                location_id: chosenLocation?._id,
-                teacher_id: chosenTeacher?._id,
+                location_id: location?._id,
+                teacher_id: teacher?._id,
                 repeat_id: repeat._id,
                 day
             })
             back()
         }
     }
+
+    const createSubject = async (name?: string, color?: number) => {
+        if(name && color) {
+            const createdSubject = await User.forge(user.id).subjects?.create({
+                name,
+                color
+            })
+            if(createdSubject) {
+                if(subjects) setSubjects(
+                    [...subjects, createdSubject]
+                )
+                setSubject(createdSubject as SubjectInput)
+            }
+        }
+        setSubjectDialogOpen(false)
+    }
+
+    const editSubject = async (name?: string, color?: number) => {
+        if(subjectEditing && subjectEditing._id && subjects && (name || color)) {
+            const subject = await User.forge(user.id).subjects?.get(subjectEditing._id)
+            if(subject) {
+                await subject.edit({
+                    name,
+                    color
+                })
+                setSubjects(subjects.map(s => s._id === subjectEditing._id ? subject : s))
+            }
+        }
+        setSubjectEditing(undefined)
+    }
+    
+    const deleteSubject = async (id?: number) => {
+        if(id && subjects) {
+            const subject = await User.forge(user.id).subjects?.get(id)
+            if(subject) await subject.delete()
+            setSubjects(subjects.filter(s => s._id !== id))
+        }
+    }
+
+    const createTeacher = async (name?: string) => {
+        if(name) {
+            const teacher = await User.forge(user.id).teachers?.create(name as string)
+            if(teacher) {
+                if(teachers) setTeachers(
+                    [...teachers, teacher]
+                )
+                setTeacher(teacher)
+            }
+        }
+    }
+
+    const editTeacher = async (name?: string) => {
+        if(teacherEditing && teacherEditing._id && teachers && name) {
+            const teacher = await User.forge(user.id).teachers?.get(teacherEditing._id)
+            if(teacher) {
+                const editedTeacher = await teacher.edit({
+                    name
+                })
+                setTeachers(teachers.map(t => t._id === teacherEditing._id ? teacher : t))
+                setTeacher(editedTeacher)
+            }
+        }
+        setTeacherEditing(undefined)
+    }
+
+    const deleteTeacher = async (id?: number) => {
+        if(id && teachers) {
+            const teacher = await User.forge(user.id).teachers?.get(id)
+            if(teacher) await teacher.delete()
+            setTeachers(teachers.filter(t => t._id !== id))
+        }
+    }
+
+    const createLocation = async (name?: string) => {
+        if(name) {
+            const location = await User.forge(user.id).locations?.create(name as string)
+            if(location) {
+                if(locations) setLocations(
+                    [...locations, location]
+                )
+                setLocation(location)
+            }
+        }
+    } 
+    
+    const editLocation = async (name?: string) => {
+        if(locationEditing && locationEditing._id && locations && name) {
+            const location = await User.forge(user.id).locations?.get(locationEditing._id)
+            if(location) {
+                const editedLocation = await location.edit({
+                    name
+                })
+                setLocations(locations.map(l => l._id === locationEditing._id ? location : l))
+                setLocation(editedLocation)
+            }
+        }
+        setLocationEditing(undefined)
+    }
+
+    const deleteLocation = async (id?: number) => {
+        if(id && locations) {
+            const location = await User.forge(user.id).locations?.get(id)
+            if(location) await location.delete()
+            setLocations(locations.filter(l => l._id !== id))
+        }
+    }
+
 
     return <>
         <NavBar name="New Timetable Entry" onBack={back}/>
@@ -124,8 +204,22 @@ export default function NewTimetable(props: Props) {
                     options={subjects} 
                     chosenSetter={setSubject} 
                     chosen={subject!} 
-                    onOpen={() => {fetchSubjects()}}
-                    CreateDialog={CreateSubject}
+                    onOpen={() => fetchSubjects()}
+                    dialog={{
+                        props: {
+                            title: "New Subject",
+                            fields: [
+                                { label: "Name", type: "text", defaultValue: newSubjectText },
+                                { label: "Color", type: "color" }
+                            ],
+                            open: subjectDialogOpen
+                        },
+                        textValueSetter: setNewSubjectText,
+                        setOpen: setSubjectDialogOpen
+                    }}
+                    save={async ([name, color]) => createSubject(name, color)}
+                    edit={(item) => setSubjectEditing(item)}
+                    _delete={(item) => deleteSubject(item._id)}
                 /> }
 
                 { locations && <CreateableAutocomplete<LocationInput>
@@ -134,6 +228,9 @@ export default function NewTimetable(props: Props) {
                     chosenSetter={setLocation}
                     chosen={location!}
                     onOpen={() => fetchLocations()}
+                    save={async ([name]) => createLocation(name)}
+                    edit={(item) => setLocationEditing(item)}
+                    _delete={(item) => deleteLocation(item._id)}
                 /> }
 
                 { teachers && <CreateableAutocomplete<TeacherInput>
@@ -141,12 +238,36 @@ export default function NewTimetable(props: Props) {
                     options={teachers}
                     chosenSetter={setTeacher}
                     chosen={teacher!}
-                    onOpen={() => fetchTeachers()}                
+                    onOpen={() => fetchTeachers()}
+                    save={async ([name]) => createTeacher(name)}
+                    edit={(item) => setTeacherEditing(item)}
+                    _delete={(item) => deleteTeacher(item._id)}
                 /> }
             </Stack>
             <Fab sx={{ position: "absolute", right: "24px", bottom: "24px" }} type="submit">
                 <Save/>
             </Fab>
         </form>
+        <EasyDialog
+            title="Edit Subject"
+            fields={[
+                { label: "Name", type: "text", defaultValue: subjectEditing?.name },
+                { label: "Color", type: "color", defaultValue: subjectEditing?.color }
+            ]}
+            open={!!subjectEditing}
+            done={([name, color]) => editSubject(name, color)}
+        />
+        <EasyDialog
+            title="Edit Location"
+            fields={[{ label: "Name", type: "text", defaultValue: locationEditing?.name }]}
+            open={!!locationEditing}
+            done={([name]) => editLocation(name)}
+        />
+        <EasyDialog
+            title="Edit Teacher"
+            fields={[{ label: "Name", type: "text", defaultValue: teacherEditing?.name }]}
+            open={!!teacherEditing}
+            done={([name]) => editTeacher(name)}
+        />
     </>
 }
