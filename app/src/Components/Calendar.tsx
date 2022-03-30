7461555214954f0737fc0cfed027fb2fbbb4cf20
jack-barr3tt <jjbdev21@gmail.com>
2022-03-30 21:47:00 +0100

    Calendar supports numerical colors

--- app/src/Components/Calendar.tsx
+++ app/src/Components/Calendar.tsx
@@ -1,17 +1,19 @@
 import { Paper, Stack, Typography } from '@mui/material'
 import { grey } from '@mui/material/colors'
+import { ColorIntToString } from '../functions';
 
 export type CalendarProps = { 
     topText: string;
     bottomText: string[];
-    accentColor: string 
+    accentColor: string|number;
 }
 
 export default function Calendar(props: CalendarProps) {
     const { topText, bottomText, accentColor } = props
+    const accentColorToUse = typeof accentColor === 'string' ? accentColor : ColorIntToString(accentColor)
     return <Paper elevation={0} sx={{ height: "96px", aspectRatio: "1", backgroundColor: grey[200], borderRadius: 3 }}>
-        <Paper sx={{ width: "100%", height: "24px", borderRadius: "12px 12px 0 0", backgroundColor: accentColor }} elevation={0}>
-            <Typography variant="body1" sx={{ textAlign: "center", height: "100%" }} color="white">{topText}</Typography>
+        <Paper sx={{ width: "100%", height: "24px", borderRadius: "12px 12px 0 0", backgroundColor: accentColorToUse }} elevation={0}>
+            <Typography variant="body1" sx={{ textAlign: "center", px: 1, height: "24px", width:"96px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }} color="white">{topText}</Typography>
         </Paper>
         { bottomText.length < 2 ?
             <Typography variant="body1" sx={{ textAlign: "center", fontSize: "2.5rem", my: "6px" }}>{bottomText[0]}</Typography>
