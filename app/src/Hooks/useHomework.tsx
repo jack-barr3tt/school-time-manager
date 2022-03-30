b9d7f334c9eebfacdcf6b084059fcce09a9a0284
jack-barr3tt <jjbdev21@gmail.com>
2022-03-30 17:11:59 +0100

    Homework hook

---
+++ app/src/Hooks/useHomework.tsx
@@ -0,0 +1,47 @@
+import { compareAsc } from "date-fns";
+import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from "react";
+import Homework from "../API/Homework";
+import { User } from "../API/Users";
+import { userContext } from "../App";
+
+type HomeworkContextValue = {
+    all: Homework[];
+    next: Homework;
+    refresh: () => void;
+}
+
+const HomeworkContext = createContext<Partial<HomeworkContextValue>>({})
+
+export const useHomework = () => useContext(HomeworkContext) as HomeworkContextValue
+
+export function HomeworkProvider (props: { children: ReactNode }) {
+    const { children } = props
+
+    const [homeworks, setHomeworks] = useState<Homework[]>()
+    const [nextHomework, setNextHomework] = useState<Homework>()
+
+    const user = useContext(userContext)
+
+    const fetchHomeworks = useCallback(async () => {
+        const tempHomeworks = await User.forge(user.id).homework?.get()
+        setHomeworks(tempHomeworks)
+        if(tempHomeworks)
+            setNextHomework(
+                tempHomeworks.sort((a, b) => compareAsc(a.due || 0, b.due || 0)).find(h => !h.complete)
+            )
+    }, [user])
+
+    useEffect(() => {
+        fetchHomeworks()
+    }, [fetchHomeworks])
+
+    const value = {
+        homeworks,
+        next: nextHomework,
+        refresh: () => fetchHomeworks()
+    }
+
+    return <HomeworkContext.Provider value={value}>
+        {children}
+    </HomeworkContext.Provider>
+}
