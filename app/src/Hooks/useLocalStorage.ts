e1918ef33c391fa1cf2c72acb93e05c57ced21cc
jack-barr3tt <jjbdev21@gmail.com>
2022-04-03 23:37:23 +0100

    Local storage hook

---
+++ app/src/Hooks/useLocalStorage.ts
@@ -0,0 +1,36 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+
+type LocalStorageProps<T> = {
+    key: string,
+    initialValue?: T;
+}
+
+type LocalStorageReturn<T> = [T, Dispatch<SetStateAction<T>>, () => void]
+
+function useLocalStorage<T>(props: LocalStorageProps<T>): LocalStorageReturn<T> {
+    const { key, initialValue } = props
+
+    const keyToUse = "stm" + key
+    const [value, setValue] = useState<T>(() => {
+
+        const json = localStorage.getItem(keyToUse)
+
+        if (json) return JSON.parse(json)
+
+        if (typeof initialValue === 'function') {
+            return initialValue()
+        } else {
+            return initialValue
+        }
+    })
+
+    const clearValue = () => sessionStorage.removeItem("stm" + key);
+
+    useEffect(() => {
+        if (value) localStorage.setItem(keyToUse, JSON.stringify(value));
+    }, [keyToUse, value, initialValue])
+
+    return [value, setValue, clearValue]
+}
+
+export default useLocalStorage;
