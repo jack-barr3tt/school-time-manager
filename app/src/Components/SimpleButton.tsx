bd8a88a8f0f8466d10a31e55815821f7f6dacafc
jack-barr3tt <jjbdev21@gmail.com>
2022-04-04 12:58:27 +0100

    Simpe button component

---
+++ app/src/Components/SimpleButton.tsx
@@ -0,0 +1,28 @@
+import { ButtonBase, ButtonBaseProps, Paper, Stack, Typography } from "@mui/material";
+import { ReactNode } from "react";
+
+type SimpleButtonProps = ButtonBaseProps & {
+    onClick: () => void;
+    icon: ReactNode;
+    text: string;
+}
+
+export default function SimpleButton(props: SimpleButtonProps) {
+    const { onClick, icon, text, ...rest } = props
+
+    return <ButtonBase 
+        sx={{
+            borderRadius: 3 
+        }}
+        onClick={onClick}
+        {...rest}
+    >
+        <Paper sx={{ py: 1, px: 3, borderRadius: 3 }} elevation={3}>
+            <Stack direction="row" spacing={1} alignItems="center">
+                {icon}
+                <Typography variant="h5">{text}</Typography>
+            </Stack>
+        </Paper>
+    </ButtonBase>
+
+}
